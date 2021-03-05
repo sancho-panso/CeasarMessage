7e4d133b0b6b284e420368ae41a995159a26c1ec
Michail Gimpelson <sancho.panso@gmail.com>
2021-03-05 19:22:44 +0200

    Add project files.

---
+++ CeasarMessage/StringCollect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Specialized;
+using System.IO;
+
+
+namespace CeasarMessage
+{
+    
+    public static class StringCollect
+    {
+        public static StringCollection dictionaryCollect(string path)
+        {
+            StringCollection wordsDictionary = new StringCollection();
+
+            FileStream dictionaryStream = File.OpenRead(path);
+            StreamReader dictionaryReader = new StreamReader(dictionaryStream);
+            do
+            {
+                string nextWord = (string)(dictionaryReader.ReadLine());
+                wordsDictionary.Add(nextWord);
+
+
+            } while (!dictionaryReader.EndOfStream);
+
+            return wordsDictionary;
+        }
+        
+
+    }
+}
---
+++ CeasarMessage/StringCompare.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeasarMessage
+{
+    class StringCompare
+    {
+        public static bool CompareStrings(string input, StringCollection words)
+        {
+            string[] inputs = input.Split(' ');
+            
+            for (int index = 0; index < inputs.Length; index++)
+            {
+                if (!words.Contains(inputs[index])) return false;
+               
+            }
+
+            
+            return true;
+            
+        }
+    }
+}
