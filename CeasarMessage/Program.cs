9675f25dacc82d71dbb1aa32382c741231da556f
Michail Gimpelson <sancho.panso@gmail.com>
2021-03-05 21:22:13 +0200

    console input

--- CeasarMessage/Program.cs
+++ CeasarMessage/Program.cs
@@ -13,39 +13,37 @@ namespace CeasarMessage
         static void Main(string[] args) {
 
             StringCollection wordsDictionary = new StringCollection();
-            
-            //Console.Write("Please enter intput file name");
-            //Console.WriteLine();
-            //string inputName = Console.ReadLine();
-
-        
-            //Console.Write("Please enter output file name");
-            //Console.WriteLine();
-            //string outputName = Console.ReadLine();
-
-            string messagePath = "C:\\Users\\Michail\\source\\repos\\" +
-                "CeasarMessage\\CeasarMessage";
-
-            //wordsDictionary = StringCollect.dictionaryCollect(messagePath, "englishWordsDictionary.txt");
             wordsDictionary = StringCollect.dictionaryCollect(".\\englishWordsDictionary.txt");
-            
 
-            FileStream messageStream = File.OpenRead(Path.Combine(messagePath, "inputMessage.txt"));
-            StreamReader messageReader = new StreamReader(messageStream);
-            string message = (string)(messageReader.ReadLine());
-            for(int key = 1; key < 27; key++)
-            {
-                string decryptedMessage = Ecrypt.DecryptStringLine(message, key);
-                decryptedMessage = decryptedMessage.ToLower();
-                if(StringCompare.CompareStrings(decryptedMessage,wordsDictionary)) Console.WriteLine(decryptedMessage);
+            Console.Write("Please enter message, try use english words only:");
+            Console.WriteLine();
+            string inputString = Console.ReadLine();
 
-               // Console.WriteLine(StringCompare.CompareStrings(decryptedMessage,wordsDictionary));
-
-            }
-           
-
+            int ecryptKey = InputNumber.InputValidNumber();
+  
+ 
+            string encryptedString = Ecrypt.EncryptStringLine(inputString, ecryptKey);
+            Console.WriteLine("Below is you encryted message");
+            Console.WriteLine(encryptedString);
+            Console.WriteLine("Press any key if you want to decrypt this message back");
             Console.ReadKey();
 
+            bool success = false;
+            for (int key = 1; key < 27; key++)
+            {
+                string decryptedMessage = Ecrypt.DecryptStringLine(inputString, key);
+                decryptedMessage = decryptedMessage.ToLower();
+                if (StringCompare.CompareStrings(decryptedMessage, wordsDictionary))
+                {
+                    Console.WriteLine(decryptedMessage);
+                    success = true;
+                }
+            }
+            if (!success)
+            {
+                Console.WriteLine("Sorry, message contains not english words \nand cannot be properly decrypted");
+            }
+            Console.ReadKey();
         }
     }
 }
